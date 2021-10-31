27ed484c95ef7dcd6509fb2526d322ce609be334
koya_watanabe <w.kou.1012@gmail.com>
2021-10-31 22:57:36 +0900

    config fzf

--- .zsh/rc/option.zsh
+++ .zsh/rc/option.zsh
@@ -6,6 +6,9 @@ setopt hist_reduce_blanks
 setopt extended_glob
 
 export FZF_DEFAULT_OPTS='
+--height 50% 
+--reverse
+--border
 --color dark,hl:51,hl+:51,fg:249,bg+:238,fg+:254
 --color info:30,prompt:123,spinner:30,pointer:37,marker:235
 '
