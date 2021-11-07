bd411f2947e653ab52751e60b88659aab7d3d7e8
koya_watanabe <w.kou.1012@gmail.com>
2021-11-07 20:12:33 +0900

    remove nouse

--- .zsh/rc/bindkey.zsh
+++
@@ -1,7 +0,0 @@
-# bindkey -v
-function select-history() {
-  BUFFER=$(history -n -r 1 | fzf --no-sort +m --query "$LBUFFER" --prompt="bck-i-search > ")
-  CURSOR=$#BUFFER
-}
-zle -N select-history
-bindkey '^R' select-history
